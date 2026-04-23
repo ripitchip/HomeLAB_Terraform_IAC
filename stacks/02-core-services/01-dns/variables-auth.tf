1b8c299dc03928f780b5e9a9fb373310db5bf400
ripitchip <thomasderudder@sfr.fr>
2026-04-23 15:24:22 +0200

    :sparkles: feat: add dns

--- stacks/02-core-services/01-dns/variables-auth.tf
+++ stacks/02-core-services/01-dns/variables-auth.tf
@@ -1,1 +1,6 @@
-../../../variables-auth.tf
+variable "proxmox_api_url"    { type = string }
+variable "proxmox_api_id"     { type = string }
+variable "proxmox_api_secret" {
+    type = string
+    sensitive = true 
+}
